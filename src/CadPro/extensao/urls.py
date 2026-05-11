f773f21f1b0f7f3bbefe9011e464d1b9441fdf65
Jonh Carvalho <jonh.carvalho@professores.ibmec.edu.br>
2026-05-11 10:58:34 -0300

    old

--- src/CadPro/extensao/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-from rest_framework.routers import DefaultRouter
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('core.urls')),
-]
